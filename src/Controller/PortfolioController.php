4490d4686a43fc6f88d6f72db75f11ae8ca1f107
tomdob1 <tomasz_dob@msn.com>
2021-12-02 18:22:00 +0000

    Created login form which hashes passwords. Added a user factory to mock test data

---
+++ src/Controller/PortfolioController.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Symfony\Component\Routing\Annotation\Route;
+
+class PortfolioController
+{
+    /**
+     * @Route("/portfolio", name="app_homepage")
+     */
+    public function index(){
+
+    }
+}
