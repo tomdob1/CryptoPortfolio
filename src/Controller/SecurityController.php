bf19ff677f0298a2e74f0b3f306237ab7ca47648
tomdob1 <tomasz_dob@msn.com>
2021-12-03 09:37:58 +0000

    Added remember me cookies, redirect and login entrypoint

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,30 @@
+<?php
+
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class SecurityController extends AbstractController
+{
+    /**
+     * @Route("/login", name="app_login")
+     */
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        return $this->render('security/login.html.twig', [
+            'error' => $authenticationUtils->getLastAuthenticationError(),
+        ]);
+    }
+
+    /**
+     * @Route("/logout", name="app_logout")
+     */
+    public function logout(AuthenticationUtils $authenticationUtils): Response
+    {
+        throw new \Exception('logout() controller reached -> this should be done via .yaml');
+    }
+}
